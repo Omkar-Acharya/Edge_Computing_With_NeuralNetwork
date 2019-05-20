3e90230dc70b75edf7d4e6a7cf1987ba1867cb9d
Omkar Mukund Acharya <omkar.acharya@uga.edu>
2019-05-19 20:48:35 -0400

    Added code to send selected img to classifier model

--- app/src/main/java/com/omkaracharya/edgecomputingwithneuralnetworks/MainActivity.java
+++ app/src/main/java/com/omkaracharya/edgecomputingwithneuralnetworks/MainActivity.java
@@ -6,21 +6,42 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MODEL_PATH = "inceptionv3_slim_2016.tflite";
+    private static final String LABEL_PATH = "imagenet_slim_labels.txt";
+    private static final int INPUT_SIZE = 299;
+
+    Classifier classifier;
+
+    Executor executor = Executors.newSingleThreadExecutor();
+
     ImageView image;
     Bitmap imgBitMap;
+    TextView resultsTextView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         image = (ImageView) findViewById(R.id.selectedImage);
-        final Button selectImage = (Button) findViewById(R.id.selectImage);
+        Button selectImage = (Button) findViewById(R.id.selectImage);
+        Button detectImage = (Button) findViewById(R.id.recognizeImage);
+        resultsTextView = findViewById(R.id.resultsTextView);
+        resultsTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        //Call to init() to load the classifier class
+        init();
 
         selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,19 +51,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(selectphoto, 1);
             }
         });
+
+        detectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Classifier.Recognition> results = classifier.recognizeImage(imgBitMap);
+                resultsTextView.setText(results.toString());
+            }
+        });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK){
+        if (resultCode == RESULT_OK) {
             Uri imgUri = data.getData();
-            if(imgUri != null){
-                try{
+            if (imgUri != null) {
+                try {
                     imgBitMap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imgUri);
                     image.setImageBitmap(imgBitMap);
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -51,12 +80,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onStop(){
+    public void onStop() {
         super.onStop();
-        if(imgBitMap != null){
+        if (imgBitMap != null) {
             imgBitMap.recycle();
             imgBitMap = null;
             System.gc();
         }
     }
+
+    void init() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    classifier = TensorFlowImageClassifier.create(
+                            getAssets(),
+                            MODEL_PATH,
+                            LABEL_PATH,
+                            INPUT_SIZE);
+                } catch (final Exception e) {
+                    throw new RuntimeException("Error initializing TensorFlow!", e);
+                }
+            }
+        });
+    }
+
 }
